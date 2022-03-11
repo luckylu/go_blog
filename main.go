3216a1ad1e12d31ea2b63e9c3f573e4a76786e19
michael.lu <michael.lu@rccchina.com>
2022-03-11 16:58:57 +0800

    refactor register database

--- main.go
+++ main.go
@@ -3,9 +3,29 @@ package main
 import (
 	_ "blog/routers"
 
+	"github.com/beego/beego/v2/client/orm"
+	"github.com/beego/beego/v2/server/web"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
+func init() {
+	// set default database
+	orm.RegisterDriver("postgres", orm.DRPostgres)
+
+	// set default RegisterDataBasedatabase
+	pg_user, _ := web.AppConfig.String("PgUser")
+	pg_pwd, _ := web.AppConfig.String("PgPassword")
+	pg_host, _ := web.AppConfig.String("PgHost")
+	pg_db, _ := web.AppConfig.String("PgDatabase")
+	db_url := "postgres://" + pg_user + ":" + pg_pwd + "@" + pg_host + "/" + pg_db + "?sslmode=disable"
+	orm.RegisterDataBase("default", "postgres", db_url)
+
+	// register model
+	// orm.RegisterModel(new(models.User), new(models.Post), new(models.Comment))
+
+	// create table
+	// orm.RunSyncdb("default", false, true)
+}
 func main() {
 	beego.Run()
 }
